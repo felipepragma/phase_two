0f56206754ba2d0dd0ddd3c998a0ed93aaf61445
Felipe Aragon <felipe.aragon@pragma.com.co>
2024-05-14 17:27:45 -0500

    fix name

--- lib/data/mappers/list_categories_mapper.dart
+++ lib/data/mappers/list_categories_mapper.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 /// [categoriesModelFromMap] method get categories from map
 /// [categoriesModelToMap] : method get string from categories
 /// 
-class ListCategoriesModel {
+class ListCategoriesMapper {
   List<String> categoriesModelFromMap(String str) => List<String>.from(json.decode(str).map((x) => x));
 
   String categoriesModelToMap(List<String> data) => json.encode(List<dynamic>.from(data.map((x) => x)));
